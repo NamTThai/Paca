82476909bbc2764662f88516a5cbec992dc8294a
NamTThai <nam.t.thai@espn.com>
2015-04-01 19:35:32 -0400

    Move to use Volley

--- app/src/main/java/com/dreamteam/paca/GalleryActivity.java
+++ app/src/main/java/com/dreamteam/paca/GalleryActivity.java
@@ -1,5 +1,6 @@
 package com.dreamteam.paca;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -8,23 +9,45 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 
 public class GalleryActivity extends ActionBarActivity {
+    public static final String TAG = GalleryActivity.class.getName();
+
+    private final static String mGetPictureAddressesUri = "http://nthai.cs.trincoll.edu/PacaServer/retrieve.php";
+
+    private RequestQueue mRequestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery_host);
 
-        GridView gridview = (GridView) findViewById(R.id.main_gallery);
-        gridview.setAdapter(new ImageAdapter(this));
+        mRequestQueue = Volley.newRequestQueue(this);
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-            }
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, mGetPictureAddressesUri,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        fetchImage(s);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        new AlertDialog.Builder(GalleryActivity.this)
+                                .setMessage("Fail to contact server")
+                                .create()
+                                .show();
+                    }
         });
+        mRequestQueue.add(stringRequest);
+
     }
 
 
@@ -40,10 +63,30 @@ public class GalleryActivity extends ActionBarActivity {
 
         switch (id) {
             case R.id.action_upload:
-                new ImageFetcher(this).execute();
+                new AlertDialog.Builder(this)
+                        .setMessage("TODO: upload function not yet implemented")
+                        .create()
+                        .show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void fetchImage(String s) {
+        new AlertDialog.Builder(this)
+                .setMessage(s)
+                .create()
+                .show();
+
+        GridView gridview = (GridView) findViewById(R.id.main_gallery);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
     }
 }
