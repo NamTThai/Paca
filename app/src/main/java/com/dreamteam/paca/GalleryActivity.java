c7cc6176718b0369e6fed172775ce28fe49cc70c
NamTThai <nam.t.thai@espn.com>
2015-04-10 00:18:31 -0400

    Delete GPSTracker

--- app/src/main/java/com/dreamteam/paca/GalleryActivity.java
+++ app/src/main/java/com/dreamteam/paca/GalleryActivity.java
@@ -1,25 +1,25 @@
 package com.dreamteam.paca;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.location.Location;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v4.util.LruCache;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,7 +28,8 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 
-public class GalleryActivity extends ActionBarActivity {
+public class GalleryActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
     public static final String TAG = GalleryActivity.class.getName();
     private static final int CAMERA_REQUEST = 1313;
 
@@ -39,11 +40,21 @@ public class GalleryActivity extends ActionBarActivity {
     private RequestQueue mRequestQueue;
     private ImageLoader mImageLoader;
 
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery_host);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        /*
         mRequestQueue = getRequestQueue();
         mImageLoader = new ImageLoader(mRequestQueue,
                 new ImageLoader.ImageCache() {
@@ -71,17 +82,36 @@ public class GalleryActivity extends ActionBarActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
                 new AlertDialog.Builder(GalleryActivity.this)
-                        .setMessage("Fail to contact server")
+                        .setMessage(R.string.cant_contact_server)
                         .create()
                         .show();
             }
         });
         jsonArrayRequest.setTag(TAG);
-        mRequestQueue.add(jsonArrayRequest);
+        mRequestQueue.add(jsonArrayRequest);*/
 
-        fetchLocationToSendToServer();
+        //fetchLocationToSendToServer();
     }
 
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "Connected");
+        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        new AlertDialog.Builder(this)
+                .setMessage(Double.toString(mLocation.getLatitude()) + " " + Double.toString(mLocation.getLongitude()))
+                .create()
+                .show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "Connection failed");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "Connection suspended");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -145,19 +175,38 @@ public class GalleryActivity extends ActionBarActivity {
     }
 
     public JSONObject fetchLocationToSendToServer() {
-        GPSTracker gpsTracker = new GPSTracker(this);
-        Location location = gpsTracker.getLocation();
         JSONObject request = new JSONObject();
 
         try {
-            if (location != null) {
-                request.put(LONGITUDE, location.getLongitude());
-                request.put(LATITUDE, location.getLatitude());
+            if (mLocation != null) {
+                request.put(LONGITUDE, mLocation.getLongitude());
+                request.put(LATITUDE, mLocation.getLatitude());
             }
         } catch (JSONException e) {
-            gpsTracker.showSettingsAlert(this);
+            showSettingsAlert(this);
         }
 
         return request;
+    }
+
+    public void showSettingsAlert(final Context context) {
+        new AlertDialog.Builder(context)
+                .setTitle(R.string.enable_gps)
+                .setMessage(R.string.enable_gps_message)
+                .setPositiveButton(R.string.button_positive, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        context.startActivity(intent);
+                    }
+                })
+                .setNegativeButton(R.string.button_negative, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .show();
     }
 }
