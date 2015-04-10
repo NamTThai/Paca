656457f5d02496d1ebdac9ad7b8f47000d5b0aba
NamTThai <nam.t.thai@espn.com>
2015-04-10 01:18:51 -0400

    Finally got location working

--- app/src/main/java/com/dreamteam/paca/GalleryActivity.java
+++ app/src/main/java/com/dreamteam/paca/GalleryActivity.java
@@ -1,29 +1,36 @@
 package com.dreamteam.paca;
 
 import android.app.AlertDialog;
-import android.content.Context;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Bitmap;
 import android.location.Location;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import android.support.v4.util.LruCache;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -37,40 +44,39 @@ public class GalleryActivity extends ActionBarActivity implements GoogleApiClien
     private static final String LONGITUDE = "longitude";
     private static final String LATITUDE = "latitude";
 
+    private static final String RESOLVING_ERROR = "Resolving error";
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+
     private RequestQueue mRequestQueue;
     private ImageLoader mImageLoader;
 
     private GoogleApiClient mGoogleApiClient;
     private Location mLocation;
+    private boolean mResolvingError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery_host);
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-
-        /*
         mRequestQueue = getRequestQueue();
-        mImageLoader = new ImageLoader(mRequestQueue,
-                new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap>
-                            cache = new LruCache<>(20);
+        mImageLoader = getImageLoader();
+        mGoogleApiClient = getGoogleApiClient();
 
-                    @Override
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
+        if (savedInstanceState != null) {
+            mResolvingError = savedInstanceState.getBoolean(RESOLVING_ERROR, false);
+        }
+    }
 
-                    @Override
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
-                    }
-                });
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(RESOLVING_ERROR, mResolvingError);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, GET_PICTURE_ADDRESS_URI,
                 new Response.Listener<JSONArray>() {
@@ -88,29 +94,70 @@ public class GalleryActivity extends ActionBarActivity implements GoogleApiClien
             }
         });
         jsonArrayRequest.setTag(TAG);
-        mRequestQueue.add(jsonArrayRequest);*/
-
-        //fetchLocationToSendToServer();
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Log.d(TAG, "Connected");
-        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        new AlertDialog.Builder(this)
-                .setMessage(Double.toString(mLocation.getLatitude()) + " " + Double.toString(mLocation.getLongitude()))
-                .create()
-                .show();
+        getRequestQueue().add(jsonArrayRequest);
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.d(TAG, "Connection failed");
+        if (!mResolvingError) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+                    mResolvingError = true;
+                } catch (IntentSender.SendIntentException e) {
+                    mGoogleApiClient.connect();
+                }
+            } else {
+                Dialog dialog = GooglePlayServicesUtil
+                        .getErrorDialog(connectionResult.getErrorCode(),
+                                this, REQUEST_RESOLVE_ERROR);
+                dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        mResolvingError = false;
+                    }
+                });
+                dialog.show();
+                mResolvingError = true;
+            }
+        }
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-        Log.d(TAG, "Connection suspended");
+        showSettingsAlert();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mResolvingError) {
+            getGoogleApiClient().connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        getGoogleApiClient().disconnect();
+        if (getRequestQueue() != null) {
+            getRequestQueue().cancelAll(TAG);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (resultCode) {
+            case REQUEST_RESOLVE_ERROR:
+                mResolvingError = false;
+                break;
+            case RESULT_OK:
+                if (!getGoogleApiClient().isConnecting() &&
+                        !getGoogleApiClient().isConnected()) {
+                    getGoogleApiClient().connect();
+                }
+                break;
+        }
     }
 
     @Override
@@ -141,14 +188,6 @@ public class GalleryActivity extends ActionBarActivity implements GoogleApiClien
         }
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (mRequestQueue != null) {
-            mRequestQueue.cancelAll(TAG);
-        }
-    }
-
     public RequestQueue getRequestQueue() {
         if (mRequestQueue == null) {
             mRequestQueue = Volley.newRequestQueue(this);
@@ -156,7 +195,35 @@ public class GalleryActivity extends ActionBarActivity implements GoogleApiClien
         return mRequestQueue;
     }
 
+    public GoogleApiClient getGoogleApiClient() {
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        return mGoogleApiClient;
+    }
+
     public ImageLoader getImageLoader() {
+        if (mImageLoader == null) {
+            mImageLoader = new ImageLoader(mRequestQueue,
+                    new ImageLoader.ImageCache() {
+                        private final LruCache<String, Bitmap>
+                                cache = new LruCache<>(20);
+
+                        @Override
+                        public Bitmap getBitmap(String url) {
+                            return cache.get(url);
+                        }
+
+                        @Override
+                        public void putBitmap(String url, Bitmap bitmap) {
+                            cache.put(url, bitmap);
+                        }
+                    });
+        }
         return mImageLoader;
     }
 
@@ -174,31 +241,15 @@ public class GalleryActivity extends ActionBarActivity implements GoogleApiClien
         imageStream.setAdapter(new ImageAdapter(this, initialAddresses));
     }
 
-    public JSONObject fetchLocationToSendToServer() {
-        JSONObject request = new JSONObject();
-
-        try {
-            if (mLocation != null) {
-                request.put(LONGITUDE, mLocation.getLongitude());
-                request.put(LATITUDE, mLocation.getLatitude());
-            }
-        } catch (JSONException e) {
-            showSettingsAlert(this);
-        }
-
-        return request;
-    }
-
-    public void showSettingsAlert(final Context context) {
-        new AlertDialog.Builder(context)
+    public void showSettingsAlert() {
+        new AlertDialog.Builder(this)
                 .setTitle(R.string.enable_gps)
                 .setMessage(R.string.enable_gps_message)
                 .setPositiveButton(R.string.button_positive, new DialogInterface.OnClickListener() {
-
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        context.startActivity(intent);
+                        startActivity(intent);
                     }
                 })
                 .setNegativeButton(R.string.button_negative, new DialogInterface.OnClickListener() {
