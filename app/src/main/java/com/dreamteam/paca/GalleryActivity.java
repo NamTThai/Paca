c10eeabba027fb61387d8c9e12ef3432dd607903
jkatz4 <jason.katz@trincoll.edu>
2015-04-12 12:23:39 -0400

    Upload
    Jason's additions for the upload process

--- app/src/main/java/com/dreamteam/paca/GalleryActivity.java
+++ app/src/main/java/com/dreamteam/paca/GalleryActivity.java
@@ -1,16 +1,24 @@
 package com.dreamteam.paca;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.util.LruCache;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -20,10 +28,21 @@ import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class GalleryActivity extends ActionBarActivity {
@@ -31,6 +50,9 @@ public class GalleryActivity extends ActionBarActivity {
     private static final int CAMERA_REQUEST = 1313;
 
     private final static String mGetPictureAddressesUri = "http://nthai.cs.trincoll.edu/PacaServer/retrieve.php";
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    String mCurrentPhotoPath;
+    private ImageView mImageView;
 
     private RequestQueue mRequestQueue;
     private ImageLoader mImageLoader;
@@ -90,18 +112,39 @@ public class GalleryActivity extends ActionBarActivity {
         switch (id) {
             case R.id.action_upload:
                 new AlertDialog.Builder(this)
-                        .setMessage("TODO: upload function not yet implemented")
+                        .setMessage("You can only upload after taking a picture!")
                         .create()
                         .show();
                 return true;
             case R.id.action_OpenCamera:
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if(cameraIntent.resolveActivity(getPackageManager()) != null){
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                    //startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                    startActivityForResult(cameraIntent,0);
+                    dispatchTakePictureIntent();
                 }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        Log.i(TAG, "onActivityResult: " + this);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
+            setPic();
+			Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+			if (bitmap != null) {
+				mImageView.setImageBitmap(bitmap);
+			try {
+					sendPhoto(bitmap);
+			} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
         }
     }
 
@@ -112,14 +155,14 @@ public class GalleryActivity extends ActionBarActivity {
             mRequestQueue.cancelAll(TAG);
         }
     }
-
+    //Requesting DataStrcuture Queue Storing the Gallery of Pictures
     public RequestQueue getRequestQueue() {
         if (mRequestQueue == null) {
             mRequestQueue = Volley.newRequestQueue(this);
         }
         return mRequestQueue;
     }
-
+    //Loading Images Onto Gallery
     public ImageLoader getImageLoader() {
         return mImageLoader;
     }
@@ -137,4 +180,172 @@ public class GalleryActivity extends ActionBarActivity {
         ListView imageStream = (ListView) findViewById(R.id.main_gallery);
         imageStream.setAdapter(new ImageAdapter(this, initialAddresses));
     }
+
+    /* This function creates an intent to take the picture and then creates
+     * the file where the photo should go
+     */
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    /* This function returns an image file created from the image
+     * in order to upload the image to the server
+     */
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        String storageDir = Environment.getExternalStorageDirectory() + "/picupload";
+        File dir = new File(storageDir);
+        if (!dir.exists())
+            dir.mkdir();
+
+        File image = new File(storageDir + "/" + imageFileName + ".jpg");
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        Log.i(TAG, "photo path = " + mCurrentPhotoPath);
+        return image;
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor << 1;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+        Matrix mtx = new Matrix();
+        mtx.postRotate(90);
+        // Rotating Bitmap
+        Bitmap rotatedBMP = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), mtx, true);
+
+        if (rotatedBMP != bitmap)
+            bitmap.recycle();
+
+        mImageView.setImageBitmap(rotatedBMP);
+
+        try {
+            sendPhoto(rotatedBMP);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    private void sendPhoto(Bitmap bitmap) throws Exception {
+        new UploadTask().execute(bitmap);
+    }
+
+    private class UploadTask extends AsyncTask<Bitmap, Void, Void> {
+
+        protected Void doInBackground(Bitmap... bitmaps) {
+            if (bitmaps[0] == null)
+                return null;
+            setProgress(0);
+
+            Bitmap bitmap = bitmaps[0];
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream); // convert Bitmap to ByteArrayOutputStream
+            InputStream in = new ByteArrayInputStream(stream.toByteArray()); // convert ByteArrayOutputStream to ByteArrayInputStream
+
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            try {
+                HttpPost httppost = new HttpPost(
+                        "http://nthai.cs.trincoll.edu/PacaServer/retrieve.php"); // server
+
+                MultipartEntity reqEntity = new MultipartEntity();
+                reqEntity.addPart("myFile",
+                        System.currentTimeMillis() + ".jpg", in);
+                httppost.setEntity(reqEntity);
+
+                Log.i(TAG, "request " + httppost.getRequestLine());
+                HttpResponse response = null;
+                try {
+                    response = httpclient.execute(httppost);
+                } catch (ClientProtocolException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                try {
+                    if (response != null)
+                        Log.i(TAG, "response " + response.getStatusLine().toString());
+                } finally {
+
+                }
+            } finally {
+
+            }
+
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            // TODO Auto-generated method stub
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);
+            Toast.makeText(GalleryActivity.this, R.string.uploaded, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
